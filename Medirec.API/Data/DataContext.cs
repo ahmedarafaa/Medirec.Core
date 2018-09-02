b7258cabe6e4366d170128f6743e40a166172755
Arafa <ahmed.arafaa@hotmail.com>
2018-09-02 17:04:41 +0300

    Initial Commit

---
+++ Medirec.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Medirec.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Medirec.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
